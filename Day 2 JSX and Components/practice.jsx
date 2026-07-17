aeb4a9c5252d529a4b8584cec0f6af9efc6e356d
Sai Krishna <aketisaikrishna1@gmail.com>
2026-07-17 17:32:24 +0530

    Add detailed notes on JSX and React component structure

---
+++ Day 2 JSX and Components/practice.jsx
@@ -0,0 +1,47 @@
+// JSX is Javascript HTML used to write the HTML code elements inside the Javascript
+
+//2. JSX is javascript which helps to write HTML in Javascript
+
+//3. <div className="first">
+    //     <Home/>
+    //     <Hero/>
+    //     <Footer/>
+    // <label htmlFor="input"></label>
+//  </div>
+
+//4. Javascript should be written inside the { login ? "logout" : "login"}, {logout()} like variables , expressions, function calls, ternary operators
+
+
+//5. React Element, usually react code conversts into javascript and then into the react element which helps to build the virtual DOM
+ //   React Component are the functions which they return the JSX
+
+ //6. JSX => Javascript => React Element => Virtual DOM => React DOM => Real DOM
+
+ //7.JSX return only one html page becasue it SPA and all the sibiling elements are wrapped inside one element
+
+ //8. React fragments are the html element in which all the component elements are wrapped up
+
+ //9. <Componenent/> is the calling the one component into another in JSX and componenet is calling of componenet function in JS
+
+ //10 Funcitonal components are written using the functional arrows and class components are used to traditional functional methods, ModErn JSX uses funcitonal 
+
+ //11. It is syntax rule followed for semantic coding
+
+ //12. React cmponenent tree :  App.tsx => index.html => index. tsx => componenets (first.jsx, second.jsx,third.jsx) are imported to index.jsx and if third.jsx  has three sub componenets they are imported into the third,jsx
+
+ //13. default export and Named export
+  // default export has only one export , for named import they have multiple export
+
+  //14. default import and named import
+
+  //15. yes
+
+  //16. JSX becomes a React Element by JSXcode turns into the javascript and then into the react element
+
+  // React Element internally is JS
+
+  // React Elements are immutable because React is based upon the immutable elements, but the elemensts can change their state
+
+  // React require one parent element in JSX becasue it create the new virtual DOM  and then compare it old virtual DOM to detect the new changes and update those changes in the React DOM witout changing the total DOM
+
+  
